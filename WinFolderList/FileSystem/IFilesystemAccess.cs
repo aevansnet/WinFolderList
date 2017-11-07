9a6b2cb7879649eb840d41ca906568adcdaad36a
Andrew Evans <andy@aevans.net>
2017-11-07 22:30:20 +0000

    Added some xml comments

--- WinFolderList/FileSystem/IFilesystemAccess.cs
+++ WinFolderList/FileSystem/IFilesystemAccess.cs
@@ -12,9 +12,32 @@ namespace WinFolderList
     /// </summary>
     public interface IFilesystemAccess
     {
+        /// <summary>
+        /// Gets some basic file information for file at given path
+        /// </summary>
+        /// <param name="path">Path to the file</param>
+        /// <returns>file information</returns>
         FileInformation GetFileInformation(string path);
+
+        /// <summary>
+        /// Gets a list of files within directory
+        /// </summary>
+        /// <param name="path">Path to query</param>
+        /// <returns>IEnumerable of file paths withing directory</returns>
         IEnumerable<string> GetFilesInDir(string path);
+
+        /// <summary>
+        /// Get a list of directories within a directory
+        /// </summary>
+        /// <param name="path">Path to query</param>
+        /// <returns>IEnumerable of directorys in directory</returns>
         IEnumerable<string> GetDirsInDir(string path);
+
+        /// <summary>
+        /// Checks to see if directory exists at given path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>Boolean indicating presence</returns>
         bool DirectoryExists(string path);
     }
 }
